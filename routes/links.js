0a29394382bcf35b1cfcfca8594ff01b952135ae
tom goodheart <20537914+t-0-m-1-3@users.noreply.github.com>
2018-03-21 15:45:21 -0400

    laid out routes framework

---
+++ routes/links.js
@@ -0,0 +1,16 @@
+/*
+ * links.js
+ * Copyright (C) 2018 t0m <t0m@asuntu>
+ *
+ * Distributed under terms of the MIT license.
+ */
+const express = require('express');
+const linksRouter = express.Router();
+const linksController = require('../controllers/links-controller');
+
+linksRouter.get('/search-a-link', linksController.index);
+linksRouter.get('/:id', linksController.show);
+linksRouter.post('new', linksController.create);
+linksRouter.delete(':id', linksController.destroy);
+
+module.exports = linksRouter;
